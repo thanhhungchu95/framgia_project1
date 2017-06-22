fb98ab2938336665d6a0303ed79ee3b2aa6357c8
Chu Thanh Hung <thanhhungchu95@gmail.com>
2017-06-23 00:14:22 +0700

    Finish basic login and logout

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,26 @@
+class SessionsController < ApplicationController
+  def new
+    return unless logged_in?
+    flash[:danger] = t ".logout_first"
+    redirect_to root_url
+  end
+
+  def create
+    user = User.find_by user_name: params[:session][:user_name].downcase
+
+    if user && user.authenticate(params[:session][:password])
+      flash[:success] = t ".welcome"
+      log_in user
+      "1" == params[:session][:remember_me] ? remember(user) : forget(user)
+      redirect_to user
+    else
+      flash.now[:danger] = t ".error"
+      render :new
+    end
+  end
+
+  def destroy
+    log_out if logged_in?
+    redirect_to root_url
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   root "statics#welcome"
   get "/help", to: "statics#help"
   get "/signup", to: "users#new"
+  get "/login", to: "sessions#new"
   post "/signup", to: "users#create"
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
   resources :users, except: :new
 end
