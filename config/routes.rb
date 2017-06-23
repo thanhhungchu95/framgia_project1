11edfcc59659d0098932f221baedf8346000a870
Chu Thanh Hung <thanhhungchu95@gmail.com>
2017-06-23 10:07:05 +0700

    Finish user posts

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,5 @@ Rails.application.routes.draw do
   post "/login", to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
   resources :users, except: :new
+  resources :posts, only: [:create, :destroy]
 end
